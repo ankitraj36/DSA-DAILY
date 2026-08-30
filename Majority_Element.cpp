492e3ab8f467d9274779bfdf509dc925368dcd67
Ankit Raj <ankitraj@Ankits-MacBook-Air.local>
2026-08-30 17:42:40 +0530

    ADD

---
+++ Majority_Element.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<int> pairsum(vector<int> nums , int target){
+    vector<int> ans;
+    int n = nums.size();
+    
+    for(int i = 0 ; i < n ; i++){
+        for(int j = i + 1 ; j < n ; j++){
+            if (nums[i] + nums[j] == target) {
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
+        }
+    }
+
+    return ans;
+}
+int main(){
+    vector<int> nums = {2 ,7 , 11 ,13};
+    int target = 13;
+
+    vector<int> ans = pairsum(nums, target);
+
+    if (!ans.empty()) {
+        cout << "Indices: " << ans[0] << " " << ans[1] << endl;
+    } else {
+        cout << "No pair found." << endl;
+    }
+
+
+
+}          
